189408d6cfc27bcaed4263e4d66f48252c317ffb
Prashant Bhattarai <prashantbhattarai696@gmail.com>
2020-12-30 19:26:39 +0545

    first commit

---
+++ server/controllers/posts.js
@@ -0,0 +1,71 @@
+// const express = require('express')
+const mongoose = require('mongoose')
+const PostMessage = require('../models/postMessage')
+
+const  getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+        res.status(200).json(postMessages);
+        
+    } catch (error) {
+        res.status(404).json({ message: error.message })
+    }
+}
+
+const createPost = async (req, res) => {
+    const post = req.body
+
+    const newPost = new PostMessage(post)
+
+    try {
+        
+        await newPost.save()
+        res.status(201).json(newPost);
+
+    } catch (error) {
+        res.status(409).json({ message: error.message })
+    }
+}
+
+const updatePost = async (req, res) => {
+    const { id: _id } = req.params
+    const post = req.body
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).send('No post with that id')
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, { ...post, _id }, { new: true })
+    // const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true }) // yelle ni kam garxa
+    
+    res.json(updatedPost)
+}
+
+const deletePost = async (req, res) => {
+    const { id } = req.params
+    const post = req.body
+
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send('No post with that id')
+    }
+
+    await PostMessage.findByIdAndDelete(id)
+    res.json({ message: 'Post deleted successfully' })
+}
+
+const likePost = async (req, res) => {
+    const { id } = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).send('No post with that id')
+    }
+
+    const post = await PostMessage.findById(id)
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true })
+
+    res.json(updatedPost)
+
+}
+
+module.exports = { getPosts, createPost, updatePost, deletePost, likePost }
